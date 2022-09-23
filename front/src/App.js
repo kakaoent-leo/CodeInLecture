11dc981913278bd7f89cb31540106b532e03938b
kakaoent-leo <leo.ee@kakaoent.com>
2022-09-24 05:01:04 +0900

    준호 커밋

--- front/src/App.js
+++ front/src/App.js
@@ -1,40 +1,64 @@
-import * as React from "react";
+import { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import { Grid } from "@mui/material";
 
 function App() {
-  var a = 1;
-  const json0 = {
-    id: 0,
-    isError: false,
-    timeStamp: 0,
-    codes: "if score >= 90",
+  const [codeImages, setCodeImages] = useState([]);
+  const [videoSrc, setVideoSrc] = useState("");
+
+  useEffect(() => {
+    var video = document.querySelector("video");
+    video.addEventListener("progress", (e) => {
+      console.log(e);
+    });
+  }, [videoSrc]);
+
+  console.log(codeImages);
+  const handleFileInput = () => {
+    const input = document.getElementById("fileInput");
+    const file = input.files[0];
+    setVideoSrc(URL.createObjectURL(file));
+    const fileName = file.name.split(".")[0];
+    const total = require("./data/init.json").total;
+
+    var codeImagesArr = [];
+    for (let i = 0; i < total; i++) {
+      const jsonName = `${fileName}_${i}.json`;
+      const imageName = `${fileName}_${i}.png`;
+      codeImagesArr.push({
+        data: require(`./data/${jsonName}`),
+        image: `./data/${imageName}`,
+      });
+    }
+    console.log(codeImagesArr);
+    setCodeImages(
+      codeImagesArr.map((item, idx) => {
+        return {
+          image: item.image,
+          code: item.data.codes,
+          isError: item.data.isError,
+        };
+      })
+    );
   };
-  const json1 = {
-    id: 1,
-    isError: false,
-    timeStamp: 5,
-    codes: 'if score >= 90:\nprint("A")',
-  };
-  const json2 = {
-    id: 2,
-    isError: false,
-    timeStamp: 10,
-    codes: 'if score >= 90:\nprint("A")\nprint("F")',
-  };
-  console.log(JSON.stringify(json0));
-  console.log(JSON.stringify(json1));
-  console.log(JSON.stringify(json2));
+
   return (
     <>
+      <input type="file" id="fileInput" onInput={handleFileInput}></input>
+      <div>a</div>
+      <div>a</div>
+      <div>a</div>
+      <div>a</div>
       <Grid container spacing={2}>
         <Grid item xs={9}>
           <video
+            id="video"
+            name="video"
             style={{ width: "100%", height: "100%" }}
             type="video/mp4"
             controls={true}
             autoPlay={true}
-            src="https://storage.googleapis.com/ent-vodka/.chunk/%EC%82%AC%EB%82%B4%EB%A7%9E%EC%84%A0_750.mp4"
+            src={videoSrc}
           ></video>
         </Grid>
         <Grid item xs={3}>
