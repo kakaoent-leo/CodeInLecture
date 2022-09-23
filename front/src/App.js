104eb836bd019926f2d52309f37dee724b4b0864
kakaoent-leo <leo.ee@kakaoent.com>
2022-09-24 01:48:07 +0900

    이영준 받아라

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,15 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  return (
+    <video
+      style={{ width: "100%", height: "100%" }}
+      type="video/mp4"
+      controls={true}
+      autoPlay={true}
+      src="https://storage.googleapis.com/ent-vodka/.chunk/%EC%82%AC%EB%82%B4%EB%A7%9E%EC%84%A0_750.mp4"
+    ></video>
+  );
 }
 
 export default App;
